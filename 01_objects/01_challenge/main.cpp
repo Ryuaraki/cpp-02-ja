a79e2277e9de7653e737a2e60daf44ce6fb39611
Ryuaraki <ryutaro.araki@woven.toyota>
2024-03-08 11:51:20 +0900

    01_challenge作成

--- 01_objects/01_challenge/main.cpp
+++ 01_objects/01_challenge/main.cpp
@@ -1,1 +1,11 @@
-// コードを入力してください
+#include <iostream>
+#include "traffic_light.h" 
+
+int main () {
+
+    std::cout << get_action(TrafficLight::red) << std::endl;
+    std::cout << get_action(TrafficLight::yellow) << std::endl;
+    std::cout << get_action(TrafficLight::green) << std::endl;
+
+    return 0;
+}
--- 01_objects/01_challenge/traffic_light.cpp
+++ 01_objects/01_challenge/traffic_light.cpp
@@ -1,1 +1,19 @@
-// コードを入力してください
+#include <iostream>
+#include <string>
+#include "traffic_light.h"
+
+std::string get_action(TrafficLight light) {
+    std::string action; 
+
+    if(light == TrafficLight::red) {
+        action = "\"Stop\"";
+    } else if (light == TrafficLight::yellow) {
+        action = "\"Caution\"";
+    } else if (light == TrafficLight::green) {
+        action = "\"Go\"";
+    } else {
+        action = "Input error.";
+    }
+
+    return action;
+}
