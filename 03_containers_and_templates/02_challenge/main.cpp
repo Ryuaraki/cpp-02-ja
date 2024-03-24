ce592966b8bc541fb5117fe470c57cb296ec31d7
Ryuaraki <ryutaro.araki@woven.toyota>
2024-03-24 18:17:30 +0900

    unit-03_Callenge02作成

--- 03_containers_and_templates/02_challenge/main.cpp
+++ 03_containers_and_templates/02_challenge/main.cpp
@@ -1,1 +1,29 @@
-// コードを入力してください
+#include <iostream>
+#include <string>
+#include <list>
+#include "stack.hpp"
+
+
+int main() {
+    Stack<int, std::vector<int>> vec_stack;
+
+    vec_stack.push(1);
+    vec_stack.push(2);
+    vec_stack.push(3);
+    
+    vec_stack.pop(); // 3
+    std::cout << vec_stack.top() << std::endl; // 2
+    std::cout << vec_stack.top() << std::endl; // 2
+
+
+    Stack<std::string, std::list<std::string>> list_stack;
+
+    list_stack.push("Hello,");
+    list_stack.push("World");
+    
+    list_stack.pop(); // World
+    list_stack.pop(); // Hello,
+    list_stack.pop(); // empty message
+
+    return 0;
+}
--- 03_containers_and_templates/02_challenge/stack.hpp
+++ 03_containers_and_templates/02_challenge/stack.hpp
@@ -1,1 +1,37 @@
-// コードを入力してください
+#include <iostream>
+#include <vector>
+
+template <typename T, typename Container = std::vector<T>>
+
+class Stack {
+public:
+    void push(const T& value) { // スタックに値を入れる
+        container.push_back(value);    
+    }
+
+    void pop() {     // スタックの一番上の値を取り出す
+        if(is_empty()) {
+            std::cerr << "Container is empty!!" << std::endl;
+            exit(EXIT_FAILURE);
+        } else {
+            std::cout << top() << std::endl;
+            container.pop_back();
+        }
+    }
+
+    T top() const {  // スタックの一番上の値を返す
+        if(is_empty()) {
+            std::cerr << "Container is empty!!" << std::endl;
+            exit(EXIT_FAILURE);
+        } else {
+            return container.back();
+        }    
+    }
+
+    bool is_empty() const {  // スタックが空の場合に返す
+        return container.empty();
+    }
+
+private:
+    Container container;
+};
